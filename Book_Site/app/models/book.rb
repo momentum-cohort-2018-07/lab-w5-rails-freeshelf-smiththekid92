bbecf7f9351cd22be85d1208001fdb781e289bdd
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-27 15:10:42 -0400

    Created books & added files to db need to add URL

---
+++ Book_Site/app/models/book.rb
@@ -0,0 +1,13 @@
+class Book < ApplicationRecord
+  validates :title, presence: true
+
+  before_validation :set_title
+
+private
+
+  def set_title
+    unless title.present?
+      self.title = "Untitled"
+    end
+  end
+end
