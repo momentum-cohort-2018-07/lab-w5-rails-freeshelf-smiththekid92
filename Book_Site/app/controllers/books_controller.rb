638f87e406c0280d38d4f01c26bc1ef6b51f263a
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-27 19:15:47 -0400

    Fixed destroy error

--- Book_Site/app/controllers/books_controller.rb
+++ Book_Site/app/controllers/books_controller.rb
@@ -40,6 +40,7 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    set_book
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: 'Book was successfully deleted.' }
