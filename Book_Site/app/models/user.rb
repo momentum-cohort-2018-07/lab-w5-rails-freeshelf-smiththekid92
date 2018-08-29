839d8c8ed145e52b751211abf91f81520e8ee7e5
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-29 11:44:55 -0400

    Added welcome page

--- Book_Site/app/models/user.rb
+++ Book_Site/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-
-  validates :username, presence: true
+  # has_secure_password
+  validates :username, presence: true, uniqueness: true
+  # validates :password, presence: true
 
   has_many :books
 
--- Book_Site/config/routes.rb
+++ Book_Site/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :books, :users
 
-  root 'books#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
