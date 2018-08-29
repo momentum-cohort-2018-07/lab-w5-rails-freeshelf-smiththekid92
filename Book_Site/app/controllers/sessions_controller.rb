8583f5618b069c8b1cf7af8124cff640e6f4e240
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-29 15:34:15 -0400

    Updated routes to direct user to books list after login. Added button for create an account. Need to add create user functionality

--- Book_Site/app/controllers/sessions_controller.rb
+++ Book_Site/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to books_path
     else
       flash[:error_message] = "Something went wrong. Please try again!"
       redirect_to new_session_path
