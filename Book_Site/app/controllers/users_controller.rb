7a172c86609667813248ff7f455fb6254710e848
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-28 20:50:30 -0400

    Updated views on main page. Fixed initial user list error. Need to fix users to display correctly on page and in db

--- Book_Site/app/controllers/users_controller.rb
+++ Book_Site/app/controllers/users_controller.rb
@@ -1,12 +1,55 @@
 class UsersController < ApplicationController
-  validates :user_id, presence: true
+  # validates :user_id, presence: true
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])  
+  end
+
+  def update
+    @user = User.find(params[:id])  
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'user was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
   def create
     @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+    
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to user_url, notice: 'User was successfully deleted.' }
+    end
   end
 
   private
   def user_params
-    params.require(:user) .permit(:name)
+    params.require(:username) .permit(:username)
   end
 end
--- Book_Site/config/routes.rb
+++ Book_Site/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'books/index'
 
-  resources :books
+  resources :books, :users
 
   root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
